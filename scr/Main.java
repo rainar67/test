f8706101834460c36ec01302a2012f2522fcc288
ppzromko <ppzromko@gmail.com>
2018-08-12 13:44:01 +0300

    1

--- scr/Main.java
+++ scr/Main.java
@@ -3,7 +3,10 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World"
+        System.out.println("Hello "
         );
+        int  x=5;
+        int[] array={1,2,3,4};
     }
+
 }
