68c1a7eb4c16e61fcd0bc0e77ae5ce9dd9a90ef3
ppzromko <ppzromko@gmail.com>
2018-08-12 13:47:48 +0300

    1

--- scr/Main.java
+++ scr/Main.java
@@ -5,8 +5,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello "
         );
-        int  x=5;
-        int[] array={1,2,3,4};
+
+        int[] array = {1, 2, 3, 4,7};
+        System.out.println("Error");
     }
 
 }
