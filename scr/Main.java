c76cae33fd6b54a79867d086f58daa3576440771
ppzromko <ppzromko@gmail.com>
2018-08-12 13:33:19 +0300

    1

---
+++ scr/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Study on 12.08.2018.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World"
+                );
+    }
+}
